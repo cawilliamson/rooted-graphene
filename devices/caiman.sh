b0e4d2abac70474daafff599f58bfb214d73c626
Christopher A. Williamson <home@chrisaw.com>
2024-12-10 11:39:42 +0000

    Attempt to add all pixel 9 series devices

---
+++ devices/caiman.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DEVICE_REPO="https://github.com/GrapheneOS/device_google_zumapro.git"
+KERNEL_BUILD_COMMAND="./build_caimito.sh --config=no_download_gki --config=no_download_gki_fips140 --lto=full"
+KERNEL_REPO="https://github.com/GrapheneOS/kernel_manifest-zumapro.git"
+KERNEL_VERSION="6.1"
+SUSFS_BRANCH="gki-android14-6.1"
+SUSFS_KERNEL_PATCH="50_add_susfs_in_gki-android14-6.1.patch"
+
+export DEVICE_REPO \
+    KERNEL_BUILD_COMMAND \
+    KERNEL_REPO \
+    KERNEL_VERSION \
+    SUSFS_BRANCH \
+    SUSFS_KERNEL_PATCH \
+    VERSION_CHECK_FILE
