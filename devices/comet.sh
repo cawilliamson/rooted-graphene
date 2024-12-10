ddadff4c0bd6249d9dd7953d421dc3916cf2f8e6
Christopher A. Williamson <home@chrisaw.com>
2024-12-10 09:10:53 +0000

    Adapt kernel build command to reflect latest Grahpene changes

--- devices/comet.sh
+++ devices/comet.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DEVICE_REPO="https://github.com/GrapheneOS/device_google_comet.git"
-KERNEL_BUILD_COMMAND="./build_comet.sh --config=use_source_tree_aosp --config=no_download_gki --lto=full"
+KERNEL_BUILD_COMMAND="./build_comet.sh --config=no_download_gki --config=no_download_gki_fips140 --lto=full"
 KERNEL_REPO="https://github.com/GrapheneOS/kernel_manifest-zumapro.git"
 KERNEL_VERSION="6.1"
 SUSFS_BRANCH="gki-android14-6.1"
